5acf75160e55bd0cedbfaad8314624916eaa3c43
Lorenzo Marchisio <Lorenzomarchisio05@icloud.com>
2022-11-28 09:39:58 +0100

    edit

--- EsAjax/js/script.js
+++ EsAjax/js/script.js
@@ -2,6 +2,6 @@ $("#first button").on("click", function () {
     $(this).siblings(".container").load("src/test.html");
 });
 
-$("#first button").on("click", function () {
-    $(this).siblings(".container").load("src/test.html");
+$("#second button").on("click", function () {
+    $(this).siblings(".container").load("src/test.html #hint");
 });
