513b3efcd4579eb47b8b645326ca70dbd5c01a70
Lorenzo Marchisio <Lorenzomarchisio05@icloud.com>
2022-12-05 09:44:11 +0100

    Es Ajax

--- EsAjax/js/script.js
+++ EsAjax/js/script.js
@@ -7,12 +7,58 @@ $("#second button").on("click", function () {
 });
 
 $("#third button").on("click", function () {
+    const container = $(this).siblings(".container");
     $.get(
         "src/compositori.json",
         function (dati) {
-            alert(dati);
+            const json = JSON.parse(dati);
+
+            console.log(json.compositori);
+
+            container.text(dati);
         },
         "text"
     );
-    $(this).siblings(".container");
+});
+
+$("#fourth button").on("click", function () {
+    $("#fourth ul li").each((i, li) => {
+        alert(this.innerText);
+        $(this).text(`${li.getAttribute("class")} nr. ${i}`);
+    });
+});
+
+$("#fifth button").on("click", function () {
+    const arrayEnum = Object.freeze(["first", "second", "third", "fourth"]);
+
+    const ul = $("<ul></ul>");
+    const div = $("<div></div>");
+
+    div.insertAfter($("#fifth h2")).append(ul);
+
+    $.each(arrayEnum, (i, value) => ul.append(`<li id="id${i}">${value}</li>`));
+});
+
+$("#sixth button").on("click", function () {
+    const tbody = $(this).siblings("table").children("tbody").html("");
+    $.get(
+        "src/compositori.json",
+        function ({ compositori }) {
+            $.each(compositori, (i, compositore) => {
+                const { nome, data_nascita, data_morte, epoca } = compositore;
+
+                $("<tr></tr>")
+                    .html(
+                        `
+                    <td>${nome}</td>
+                    <td>${data_nascita}</td>
+                    <td>${data_morte}</td>
+                    <td>${epoca}</td>
+                `
+                    )
+                    .appendTo(tbody);
+            });
+        },
+        "json"
+    );
 });
